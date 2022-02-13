3a42804e17f4ead115bd5d7ccb23552af6a85b3b
miyasan_0301 <miyasan.19990301@gmail.com>
2022-02-13 17:37:36 +0900

    🏷️ style用型定義

--- src/types/style.d.ts
+++ src/types/style.d.ts
@@ -1,13 +1,28 @@
-import type { StyleProp, TextStyle, ViewStyle } from "react-native";
+import type { FlexStyle, StyleProp, TextStyle, TransformsStyle, ViewStyle } from "react-native";
+
+import type { theme } from "~/styles/theme";
+
+type ThemeProps = keyof typeof theme[keyof typeof theme];
+type TextTheme = "text0" | "text1" | "text2" | "text3" | "primary" | "accent";
+type BgTheme = "bg0" | "bg1" | "bg2" | "bg3" | "primary" | "accent";
+
+type OutlineStyle = Pick<ViewStyle, keyof FlexStyle | keyof TransformsStyle>;
 
 export type StyleProps = {
-  lightTextColor?: string;
-  darkTextColor?: string;
+  // Text
   textStyle?: StyleProp<TextStyle>;
-
-  lightBgColor?: string;
-  darkBgColor?: string;
+  textTheme?: Extract<ThemeProps, TextTheme>;
+  lightText?: string;
+  darkText?: string;
+  // Views
   bgStyle?: StyleProp<ViewStyle>;
-
-  outlineStyle?: StyleProp<ViewStyle>;
+  bgTheme?: Extract<ThemeProps, BgTheme>;
+  lightBg?: string;
+  darkBg?: string;
+  // View Outline
+  outlineStyle?: StyleProp<OutlineStyle>;
 };
+
+export type TextStyleProps = Pick<StyleProps, "textStyle" | "textTheme" | "lightText" | "darkText">;
+
+export type ViewStyleProps = Pick<StyleProps, "bgStyle" | "bgTheme" | "lightBg" | "darkBg">;
